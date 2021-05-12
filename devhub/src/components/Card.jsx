e58234fdeee735cae141350795b864683f155827
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-11 20:53:33 -0400

    some styles

--- devhub/src/components/Card.jsx
+++ devhub/src/components/Card.jsx
@@ -4,13 +4,15 @@ import { baseURL, config } from "../services"
 
 function Card(props) {
   return (
-    <div className="card-card-container">
-      
+    <div className="card-container">
       <div className="card">
         {/* <a href={props.card.field.link}></a> */}
-        <h4>{props.card.fields.title}</h4>
+        <h3>{props.card.fields.title}</h3>
         <h4>{props.card.fields.description}</h4>
-        <h4>{props.card.fields.link}</h4>
+        {/* <button>
+          <a href={props.cards.fields.url}>Go To</a>
+        </button> */}
+        <h4></h4>
         <Link
           to={`/resource/${props.card.fields.category}/edit/${props.card.id}`}
         >
