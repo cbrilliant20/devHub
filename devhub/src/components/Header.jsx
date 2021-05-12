3cf06cba551198756dcb30c3e0d80ee4545235cc
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-05-12 17:06:23 -0400

    styles

--- devhub/src/components/Header.jsx
+++ devhub/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import Burger from "./Burger"
+import { Link } from "react-router-dom"
 
 function Header() {
   return (
     <header>
-      <h1>devHub</h1>
+      <Link to="/">
+        <h1>devHub</h1>
+      </Link>
       <Burger />
     </header>
   )
